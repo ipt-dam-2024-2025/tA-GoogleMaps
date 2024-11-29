f050971681a9d0e8373e34650393119423865d2c
jcnpereira <casimiro@ipt.pt>
2024-11-29 15:12:21 +0000

    adição de ´dependências' ao ficheiro Gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,9 +33,18 @@ android {
     kotlinOptions {
         jvmTarget = "11"
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
+
 }
 
 dependencies {
+
+    // implementation ("com.google.android.gms:play-services-maps:19.0.0")
+    implementation (libs.play.services.maps)
 
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
